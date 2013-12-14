fb3c80f021f96b118f5b0fbadb21ece331d1d8c8
GuySh <rakzevel1@gmail.com>
2013-12-14 18:22:10 +0200

    red : negativeNumberWillThrowAnException()

--- string_calculator_TDD/src/string_calculator_TDD/string_calculator_test.java
+++ string_calculator_TDD/src/string_calculator_TDD/string_calculator_test.java
@@ -6,9 +6,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class string_calculator_test {
-	
+
 	private calculator calc;
-	
+
 	@Before
 	public void init()
 	{
@@ -21,13 +21,13 @@ public class string_calculator_test {
 		calculator calc=new calculator();
 		assertEquals(calc.Add(""), 0);
 	}
-	
+
 	@Test
 	public void oneNumber()
 	{
 		assertEquals(calc.Add("1"), 1);
 	}
-	
+
 	@Test
 	public void TwoNumbers()
 	{
@@ -39,18 +39,26 @@ public class string_calculator_test {
 	{
 		assertEquals(calc.Add("1,2,3,4"), 10);
 	}
-	
+
 	@Test
 	public void lineBetweenNumbers()
 	{
 		assertEquals(calc.Add("1,2\n3,4"), 10);
 	}
-	
+
 	@Test
 	public void differentDelimiters()
 	{
 		assertEquals(calc.Add("//;\n1;2"), 3);
 	}
+
+	@Test
+	public void negativeNumberWillThrowAnException() 
+	{
+
+		assertEquals(calc.Add("-1,-3,2"), 1);
+	}
+
 
 
 }
